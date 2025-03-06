9674a0c3f02f525552f61f545d00d3601f6fd5c8
Alp Emre <78408018+alpemre8@users.noreply.github.com>
2025-03-06 09:28:25 +0300

    Update feature_extractor.py

--- models/feature_extractor.py
+++ models/feature_extractor.py
@@ -6,7 +6,7 @@ def create_feature_extractor():
   base_model = keras.applications.EfficientNetB0(include_top=False)
   base_model.trainable = False
 
-  inputs = keras.layers.Inputs(shape=(224, 224, 3), name="input_layer")
+  inputs = keras.layers.Input(shape=(224, 224, 3), name="input_layer")
   x = base_model(inputs, training=False)
   x = keras.layers.GlobalAveragePooling2D(name="pooling_layer_2d")(x)
   x = keras.layers.Dense(units=101)(x)
